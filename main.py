39c0a9494d86e4811a5a1792209c5a3f46f5fc3a
Topchii Sofiia <sofiia@Air-Topchii.sw.kse.local>
2022-12-01 15:06:28 +0200

    fixed double words

--- main.py
+++ main.py
@@ -2,12 +2,29 @@ import words_fetcher
 import random
 
 
-def congratulate_user():
+def congratulate_user(guesses):
     print(f"Congratulations, you won! your words: {guesses}")
+    print("=============================")
+    print("= Congratulations! You won! =")
+    print("=============================")
 
 
 def is_game_over():
     return guessed == WORDS_TO_WIN or errors == ERRORS_TO_LOSE
+
+
+def guess_is_valid(candidate, errors):
+    for letter in candidate:
+        if letter not in word:
+            print(f"You can not use letter {letter}")
+            errors += 1
+            return False, errors
+        count = word.count(letter)
+        if count < candidate.count(letter):
+            print(f"You can use letter {letter} only {count} times")
+            errors += 1
+            return False, errors
+    return True
 
 
 guessed = 0
@@ -28,13 +45,20 @@ print(f"Your word is '{word}'")
 
 while not is_game_over():
     guess = input("Your next take: ")
+
+    if not guess_is_valid(guess):
+        continue
+
     if guess in full_list:
         guessed += 1
         guesses.append(guess)
         if guessed == WORDS_TO_WIN:
-            congratulate_user()
+            congratulate_user(guesses)
             exit()
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+    if guess in guesses:
+        print("you already used this word")
+        errors += 1
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
